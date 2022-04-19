eca3a695612bbf00d7652a0a92160ffe825c64d4
jlurie22 <103161520+jlurie22@users.noreply.github.com>
2022-04-18 22:30:18 -0400

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function Header({ name }) {
   return (
     <header className="pt-20 pb-12">
-      <img src="corona-logo.png" className="w-12 h-12 mx-auto mb-4" />
+      <img src="corona-logo.png" class="w-12 h-12 mx-auto mb-4" />
       <p className="text-2xl dark:text-white text-center">
         <Link href="/">
           <a>{name}</a>
